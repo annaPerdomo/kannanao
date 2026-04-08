1a5b53246fa1e2c1c9ec9e0908a4cbcc0728252e
Anna Perdomo <annamaliaperdomo@gmail.com>
2026-04-07 23:37:42 -0700

    fix: Attempt to fix deployment build errors

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import type { AppProps } from "next/app";
+import Providers from "@/app/providers";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Providers>
+      <Component {...pageProps} />
+    </Providers>
+  );
+}
